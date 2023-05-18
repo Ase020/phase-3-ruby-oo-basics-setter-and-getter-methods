2d52ce5c646017fc940dbab0af4767632f10bc8d
felixNyalenda <felixasejunior@gmail.com>
2023-05-18 14:14:33 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+  attr_accessor :name
+  attr_accessor :breed
+  def name=(name)
+    @name = name
+  end
 
+  # def name
+  #   puts @name
+  # end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # def breed
+  #   @breed
+  # end
 end
+
+fido = Dog.new
+fido.name = 'Fido'
+fido.breed = 'Shepherd'
+
+puts fido.name, fido.breed
